26a404b9d651dc0f5c4ebe849d248928b40008d7
marcogloriavix <marcogloriavix@gmail.com>
2024-04-29 16:37:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,29 @@
 //"O Herói de nome **{nome}** está no nível de **{nivel}**"
 
 let nickName = "João Grandão"
-let nivelExperiencia = 7820
+let XP = 15000
 console.log(nickName)
 
 
-
+if (XP < 1000) {
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Ferro.");
+} else if (XP >= 1000 && XP <= 2000) {
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Bronze.");
+} else if (XP >= 2000 && XP <= 5000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Prata.");
+}
+else if (XP >= 5000 && XP <= 7000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Ouro.");
+}
+else if (XP >= 7000 && XP <= 8000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Platina.");
+}
+else if (XP >= 8000 && XP <= 9000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Ascendente.");
+}
+else if (XP >= 9000 && XP <= 10000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Imortal.");
+}
+else if (XP > 10000){
+    console.log("O Herói de nome " + nickName +  " está no nível de " + "Radiante.");
+}
