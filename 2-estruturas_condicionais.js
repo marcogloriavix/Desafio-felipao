b0e97b6b742fea45413d3ed25062ef732bbb7fda
marcogloriavix <marcogloriavix@gmail.com>
2024-04-30 18:34:50 -0300

    Adicionando Estruturas condicionais IF Else

---
+++ 2-estruturas_condicionais.js
@@ -0,0 +1,47 @@
+// estruturas condicionais if, else if, else
+// www.w3schools.com
+
+let ligado = false
+
+if (ligado == true) {
+    console.log ("executou...ué !")
+}
+
+console.log("fim!")
+
+//------------------------------------------
+// Estrutura if -else
+
+let possuiOvos = true
+let itensComprados =""
+
+if(possuiOvos){
+    itensComprados = "leite"
+}
+else{
+    console.log("passou na sessão de congelados")
+    itensComprados  = "leite"
+}
+
+//-----------------------------------------
+// Estrutura ELSE-IF
+let nivelFome = 1
+
+if(nivelFome === 1){
+    console.log("nem consegue respirar!!!")
+
+} else if (nivelFome ===2) {
+    console.log("está fraco !!!")
+    
+} else if (nivelFome === 3) {
+    console.log ("está mais ou menos")
+    
+}else{
+    console.log ("Você come demais")
+}
+
+//------------------------------------------
+
+
+
+
