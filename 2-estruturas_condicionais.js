f22b848edf5f2ed6b71e36528554456175484000
marcogloriavix <marcogloriavix@gmail.com>
2024-05-01 22:33:46 -0300

    Update 2-estruturas_condicionais.js

--- 2-estruturas_condicionais.js
+++ 2-estruturas_condicionais.js
@@ -41,6 +41,30 @@ if(nivelFome === 1){
 }
 
 //------------------------------------------
+// Switch Case - multiplas opções em uma única pergunta
+//switch  / case/ break/ default
+
+let fruta  = "banana"
+switch (fruta){   // dentro da condição do switch case, posso colocar numeros
+    case "laranja": // dentro do "case", posso fazer o meu código com várias estruturas
+        console.log("suco de laranja")
+        break
+
+    case "banana": // dentro do "case", posso fazer o meu código com várias estruturas
+    case "morango":
+        console.log("vitamina de banana ou morango")
+        break
+
+    case "maça":
+        console.log("vitamina de maça")
+        break
+    
+    default:
+    console.log ("suco genérico")
+}
+
+// *************************************************
+//************************************************* */
 
 
 
